026fa0623187cff66e9a6e9c862de72663fd6186
Refi Lexmana <refi.lexmana13@gmail.com>
2026-02-24 10:34:23 +0700

    edit di baseApi.java

--- src/test/java/com/refi/api/baseApi/baseApi.java
+++ src/test/java/com/refi/api/baseApi/baseApi.java
@@ -6,11 +6,31 @@ import org.json.JSONObject;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class baseApi {
-    private static final Dotenv dotenv = Dotenv.load();
-    private String TOKEN = dotenv.get("TOKEN");
 
-    private String URL = "https://gorest.co.in/public/v2/users";
+// Disini masih Gagal
+//    private static final Dotenv dotenv = Dotenv.load();
+//    private String TOKEN = dotenv.get("TOKEN");
+//
+//    private String URL = "https://gorest.co.in/public/v2/users";
 
+    private String TOKEN;
+    private String URL;
+
+    public baseApi() {
+
+    Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
+
+    // ambil dari GitHub Actions env dulu
+    TOKEN = System.getenv("TOKEN") != null
+            ? System.getenv("TOKEN")
+            : dotenv.get("TOKEN");
+
+    URL = System.getenv("BASE_URL") != null
+            ? System.getenv("BASE_URL")
+            : "https://gorest.co.in/public/v2/users";
+}
 
     public Response postCreateUser(String name, String email, String gender, String status) {
         JSONObject body = new JSONObject();
